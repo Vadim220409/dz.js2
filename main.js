cfb369e665885cdf75aeea8777405155f5915fc6
Vadim220409 <107919314+Vadim220409@users.noreply.github.com>
2023-02-12 12:45:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+let a = prompt ()
+let b = prompt ()
+let a1 = Number(a)
+let b1 = Number(b)
+let c = a1 + b1
+alert(c);
+if (c > 10) {
+    alert("ok");
+}
+else {
+    alert("no")
+}
+
+
+let username1 = prompt()
+let username2 = "Andrew"
+if (username1 == username2) {
+    alert("Привіт")
+}
+else {
+    alert("Хто ти?")
+}
